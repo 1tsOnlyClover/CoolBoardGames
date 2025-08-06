28f1031f1f014def7e3b4efc3fdaee080e7530cd
Cason Terry <cterry@student.neumont.edu>
2025-08-05 19:39:23 -0600

    Finished Logical Part of connect 4

--- connectFour/fourGame.js
+++ connectFour/fourGame.js
@@ -22,12 +22,24 @@ function printBoard() { // tool to log the current board state in the console
 }
 
 function addToken(type,colIndex) { // will place a peice of the given type in the given col
-    
+    let rowIndex = board.length - 1;
+    try {
+        while (true) {
+            if (board[rowIndex][colIndex] == 0) {
+                board[rowIndex][colIndex] = type;
+                return;
+            }
+            rowIndex--;
+        }
+    } catch {}
 }
 
 // searches the board to find 4 in a row, will return 1 or -1 depending on who won or 0 if there is no winner
 function findWinner() { 
     const winTotal = findHorizontalWin() + findVerticalWin() + findDescWin() + findAscWin();
+    if (winTotal == 0) {
+        return 0;
+    }
     return winTotal / Math.abs(winTotal);
 }
 
@@ -35,19 +47,103 @@ function findWinner() {
 function findHorizontalWin() {
     for (let row = 0; row < board.length; row++) {
         for (let col = 0; col < board[row].length; col++) {
-            
+            const tokenType = board[row][col];
+            if (tokenType != 0) {
+                let tokenCount = 1;
+                try {
+                    while (true) {
+                        if (board[row][col + tokenCount] == tokenType) {
+                            tokenCount++;
+                            if (tokenCount >= 4) {
+                                return tokenType;
+                            }
+                        } else {
+                            break
+                        }
+                    }
+                } catch {}
+            }
         }
     }
+    return 0;
 }
 
 function findVerticalWin() {
-
+    for (let row = 0; row < board.length; row++) {
+        for (let col = 0; col < board[row].length; col++) {
+            const tokenType = board[row][col];
+            if (tokenType != 0) {
+                let tokenCount = 1;
+                try {
+                    while (true) {
+                        if (board[row + tokenCount][col] == tokenType) {
+                            tokenCount++;
+                            if (tokenCount >= 4) {
+                                return tokenType;
+                            }
+                        } else {
+                            break
+                        }
+                    }
+                } catch {}
+            }
+        }
+    }
+    return 0;
 }
 
 function findDescWin() {
-
+    for (let row = 0; row < board.length; row++) {
+        for (let col = 0; col < board[row].length; col++) {
+            const tokenType = board[row][col];
+            if (tokenType != 0) {
+                let tokenCount = 1;
+                try {
+                    while (true) {
+                        if (board[row - tokenCount][col + tokenCount] == tokenType) {
+                            tokenCount++;
+                            if (tokenCount >= 4) {
+                                return tokenType;
+                            }
+                        } else {
+                            break
+                        }
+                    }
+                } catch {}
+            }
+        }
+    }
+    return 0;
 }
 
 function findAscWin() {
+    for (let row = 0; row < board.length; row++) {
+        for (let col = 0; col < board[row].length; col++) {
+            const tokenType = board[row][col];
+            if (tokenType != 0) {
+                let tokenCount = 1;
+                try {
+                    while (true) {
+                        if (board[row + tokenCount][col + tokenCount] == tokenType) {
+                            tokenCount++;
+                            if (tokenCount >= 4) {
+                                return tokenType;
+                            }
+                        } else {
+                            break
+                        }
+                    }
+                } catch {}
+            }
+        }
+    }
+    return 0;
+}
 
+function clearBoard() {
+    for (let row = 0; row < board.length; row++) {
+        for (let col = 0; col < board[row].length; col++) {
+            board[row][col] = 0;
+        }
+    }
 }
