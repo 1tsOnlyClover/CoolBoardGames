c903bc33cf4dbeb77728ed5215b85de0d2aa1663
Cason Terry <cterry@student.neumont.edu>
2025-08-05 18:47:33 -0600

    added some javascript and basic hmtl

---
+++ js/site.js
@@ -0,0 +1,25 @@
+const board = [
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+];
+// 0 will be an unfilled space, 1 will be red, -1 will be black
+// board[row][colum]
+
+function printBoard() {
+    let boardText = "";
+    for (let row = 0; row < board.length; row++) {
+        for (let col = 0; col < board[row].length; col++) {
+            boardText += board[row][col] + " ";
+        }
+        boardText += '\n';
+    }
+    console.log(boardText)
+}
+
+function addToken(type) {
+    
+}
