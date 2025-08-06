ce3f5c001beebdf687355f139a2415140591fdac
Cason Terry <cterry@student.neumont.edu>
2025-08-05 18:49:15 -0600

    added some javascript and basic hmtl

--- js/site.js
+++ js/site.js
@@ -1,3 +1,4 @@
+
 const board = [
     [0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0],
@@ -9,7 +10,7 @@ const board = [
 // 0 will be an unfilled space, 1 will be red, -1 will be black
 // board[row][colum]
 
-function printBoard() {
+function printBoard() { // tool to log the current board state in the console
     let boardText = "";
     for (let row = 0; row < board.length; row++) {
         for (let col = 0; col < board[row].length; col++) {
@@ -20,6 +21,33 @@ function printBoard() {
     console.log(boardText)
 }
 
-function addToken(type) {
+function addToken(type,colIndex) { // will place a peice of the given type in the given col
     
+}
+
+// searches the board to find 4 in a row, will return 1 or -1 depending on who won or 0 if there is no winner
+function findWinner() { 
+    const winTotal = findHorizontalWin() + findVerticalWin() + findDescWin() + findAscWin();
+    return winTotal / Math.abs(winTotal);
+}
+
+// each of these 4 functions will check if a win in each of these directions exist, and will return 1 -1 or 0 depending on what they find
+function findHorizontalWin() {
+    for (let row = 0; row < board.length; row++) {
+        for (let col = 0; col < board[row].length; col++) {
+            
+        }
+    }
+}
+
+function findVerticalWin() {
+
+}
+
+function findDescWin() {
+
+}
+
+function findAscWin() {
+
 }
