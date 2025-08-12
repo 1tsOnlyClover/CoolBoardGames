6bcf2f81ece796719104b012edafecbd2b00f267
Cason Terry <cterry@student.neumont.edu>
2025-08-12 16:55:09 -0600

    moving BASIC Peices works

--- checkers/checkersGame.js
+++ checkers/checkersGame.js
@@ -1,25 +1,36 @@
 board = [
-    [ 0,-1, 0,-1, 0,-1, 0,-1],
-    [-1, 0,-1, 0,-1, 0,-1, 0],
-    [ 0,-1, 0,-1, 0,-1, 0,-1],
-    [ 0, 0, 0, 0, 0, 0, 0, 0],
-    [ 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 0, 1, 0, 1, 0, 1, 0, 1],
     [ 1, 0, 1, 0, 1, 0, 1, 0],
     [ 0, 1, 0, 1, 0, 1, 0, 1],
-    [ 1, 0, 1, 0, 1, 0, 1, 0]
+    [ 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 0, 0, 0, 0, 0, 0, 0, 0],
+    [-1, 0,-1, 0,-1, 0,-1, 0],
+    [ 0,-1, 0,-1, 0,-1, 0,-1],
+    [-1, 0,-1, 0,-1, 0,-1, 0]
 ];
+
+function printBoard() {
+    let rowText = "";
+    for (let row = 0; row < board.length; row++) {
+        for (let col = 0; col < board[row].length; col++) {
+            rowText += (board[row][col] == -1 ? "" : " ") + board[row][col] + " ";
+        }
+        rowText += "\n";
+    }
+    console.log(rowText);
+}
 
 function resetBoard() {
     board = [
-    [ 0,-1, 0,-1, 0,-1, 0,-1],
-    [-1, 0,-1, 0,-1, 0,-1, 0],
-    [ 0,-1, 0,-1, 0,-1, 0,-1],
-    [ 0, 0, 0, 0, 0, 0, 0, 0],
-    [ 0, 0, 0, 0, 0, 0, 0, 0],
-    [ 1, 0, 1, 0, 1, 0, 1, 0],
-    [ 0, 1, 0, 1, 0, 1, 0, 1],
-    [ 1, 0, 1, 0, 1, 0, 1, 0]
-];
+        [ 0, 1, 0, 1, 0, 1, 0, 1],
+        [ 1, 0, 1, 0, 1, 0, 1, 0],
+        [ 0, 1, 0, 1, 0, 1, 0, 1],
+        [ 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 0, 0],
+        [-1, 0,-1, 0,-1, 0,-1, 0],
+        [ 0,-1, 0,-1, 0,-1, 0,-1],
+        [-1, 0,-1, 0,-1, 0,-1, 0]
+    ];
 }
 
 class Move {
@@ -36,8 +47,15 @@ class Move {
     move() {
         movePiece(this.originX,this.originY,this.destinationX,this.destinationY);
         if (this.captured) {
-            board[this.destinationX][this.destinationY] = 0;
+            board[this.capturedX][this.capturedY] = 0;
         }
+    }
+
+    display() {
+        console.log("OriginX: " + this.originX);
+        console.log("OriginY: " + this.originY);
+        console.log("DestinationX: " + this.destinationX);
+        console.log("DestinationY: " + this.destinationY);
     }
 }
 
@@ -48,8 +66,75 @@ function movePiece(x,y,X,Y) {
     const piece = board[x][y];
     board[x][y] = 0;
     board[X][Y] = piece;
+
+    const type = Math.abs(piece);
+    if (type == 1) {
+        if (piece == 1) {
+            if (X == board.length - 1) {
+                board[X][Y] == 2;
+            }
+        } else if (piece == -1) {
+            if (X == 0) {
+                board[X][Y] == -2;
+            }
+        }
+    }
 }
 
-function getMoves() {
+function getSign(num) {
+    return Math.abs(num) / num;
+}
 
+function checkMove(originX, originY, sign, shiftY, onlyCapture) {
+    try {
+        const spaceType = board[originX + sign][originY + shiftY];
+        if (spaceType == undefined) {
+            return null;
+        }
+        if ((spaceType == 0) && (!onlyCapture)) {
+            return new Move(originX, originY, originX + sign, originY + shiftY, false, -1, -1);
+        } else if (getSign(spaceType) == sign * -1) {
+            const spaceTypeJump = board[originX + sign + sign][originY + shiftY + shiftY];
+            if (spaceTypeJump == undefined) {
+                return null;
+            }
+            if (spaceTypeJump == 0) {
+                return new Move(originX, originY, originX + sign + sign, originY + shiftY + shiftY, true, originX + sign, originY + shiftY);
+            }
+        } else {
+            return null;
+        }
+    } catch {
+        return null;
+    }
+}
+
+
+function getMoves(originX, originY, onlyCapture) {
+    const peice = board[originX][originY];
+    const sign = getSign(peice);
+    const type = Math.abs(peice);
+    let moves = [];
+
+    if (type == 1 || type == 2) {
+        const move1 = checkMove(originX, originY, sign, 1, onlyCapture);
+        const move2 = checkMove(originX, originY, sign, -1, onlyCapture);
+        if (move1 != null) {
+            moves.push(move1);
+        }
+        if (move2 != null) {
+            moves.push(move2);
+        }
+    } 
+    if (type == 2) {
+        const move3 = checkMove(originX, originY, sign * -1, 1, onlyCapture);
+        const move4 = checkMove(originX, originY, sign * -1, -1, onlyCapture);
+        if (move3 != null) {
+            moves.push(move3);
+        }
+        if (move4 != null) {
+            moves.push(move4);
+        }
+    }
+    return moves
 }
