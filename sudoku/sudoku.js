1a9658e7447aac32eed5939d3fcb227a6a489d03
HailexTheHuman <cmyers@student.neumont.edu>
2025-08-13 15:38:14 -0600

    sudoku logic

--- sudoku/sudoku.js
+++ sudoku/sudoku.js
@@ -79,6 +79,31 @@ const board = [
     [0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0]
 ];
+
+let boardFilling = [
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0]
+];
+
+let boardPlayer = [
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0]
+];
+
 // 0 will be an unfilled space
 // board[row][column]
 
@@ -133,7 +158,10 @@ function setupBoard() {
             ctx.stroke();
         }
     }
-    fillBoard();
+    let boardFilled = fillBoard();
+    while (!boardFilled) {
+        boardFilled = fillBoard();
+    }
     printBoard();
     for (let row = 0; row < board.length; row++) {
         for (let col = 0; col < board.length; col++) {
@@ -144,44 +172,148 @@ function setupBoard() {
     }
 }
 
+function makePlayerBoard() {
+    for (let row = 0; row < boardPlayer.length; row++) {
+        for (let col = 0; col < boardPlayer[row].length; col++) {
+            boardPlayer[row][col] = board[row][col];
+        }
+    }
+    for (let row = 0; row < boardPlayer.length; row++) {
+        for (let col = 0; col < boardPlayer[row].length; col++) {
+            if (boardPlayer[row][col] != 0) {
+                placeNumberBoard(row, col, boardPlayer[row][col]);
+            }
+        }
+    }
+}
 
 function placeNumberBoard(rowIndex,colIndex,number) {
     ctx.fillStyle = "black";
-    ctx.font = "50px Arial";
+    ctx.font = "50px Comics Sans MS";
     ctx.fillText(number, colIndex * 100 + 35, rowIndex * 100 + 65);
 }
 
 setupBoard(); // initially set up the board
-
-function rowValidity(col,number) {
-    // will return true if the number is not already in the row
-    for (let i = 0; i < board[col].length; i++) {
-        if (board[col][i] == number) {
-            return false;
+    
+function newPuzzle() {
+    let numGiven = 0;
+    for (let i = 0; i < numGiven; i++) {
+        let row = Math.floor(Math.random() * 9);
+        let col = Math.floor(Math.random() * 9);
+        if (boardFilling[row][col] == 0) {
+            if (!fillSpace(row, col)) {
+                setFillingFromBoard();
+                i = 0;
+            }
         }
     }
-    return true;
+    setBoardFromFilling();
 }
 
-function colValidity(row,number) {
-    // will return true if the number is not already in the column
-    for (let i = 0; i < board.length; i++) {
-        if (board[i][row] == number) {
-            return false;
+function attemptPuzzle() {
+    while (true) {
+        if (fillBoard()) {
+            break;
         }
     }
-    return true;
 }
 
+function setBoardFromFilling() {
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            board[i][j] = boardFilling[i][j];
+        }
+    }
+}
+
+function setFillingFromBoard() {
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            boardFilling[i][j] = board[i][j];
+        }
+    }
+}
 
 function fillBoard() {
-    for (let row = 0; row < 9; row++) {
-        for (let col = 0; col < 9; col++) {
-            let num = 0;
-            do {
-                num = Math.floor(Math.random() * 9) + 1;
-            } while (!rowValidity(row, num) || !colValidity(col, num))
-            board[row][col] = num;
+    setFillingFromBoard();
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            if (boardFilling[i][j] == 0) {
+                if (!fillSpace(i, j)) {
+                    return false;
+                }
+            }
         }
     }
+    setBoardFromFilling();
+    return true;
+}
+
+function fillSpace(row, col) {
+    let isValid = true;
+    let iteration = 0;
+    do {
+        isValid = true;
+        boardFilling[row][col] = Math.floor(Math.random() * 9) + 1;
+        for (let i = 0; i < 9; i++) {
+            if (i != row) {
+                if (boardFilling[i][col] == boardFilling[row][col]) {
+                    isValid = false;
+                }
+            }
+        }
+        for (let i = 0; i < 9; i++) {
+            if (i != col) {
+                if (boardFilling[row][i] == boardFilling[row][col]) {
+                    isValid = false;
+                }
+            }
+        }
+        if (!threeByThree(row, col)) {
+            isValid = false;
+        }
+        iteration++;
+        if (iteration > 30) {
+            return false;
+        }
+    } while (!isValid);
+    return isValid;
+}
+
+function threeByThree(row, col) {
+    let nonCoderRow = (row+1);
+    let nonCoderCol = (col+1);
+    let startRow = 0;
+    let startCol = 0;
+    startRow = getStartPoint(row, nonCoderRow, startRow);
+    startCol = getStartPoint(col, nonCoderCol, startCol);
+    let endRow = startRow+2;
+    let endCol = startCol+2;
+    for (let i = startRow; i <= endRow; i++) {
+        for (let j = startCol; j <= endCol; j++) {
+            if (boardFilling[i][j] != 0 && (i != row || j != col)) {
+                if (boardFilling[i][j] == boardFilling[row][col]) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+function getStartPoint(col, nonCoder, start) {
+    switch (nonCoder%3) {
+        case 0:
+            start = col-2;
+            break;
+        case 1:
+            start = col;
+            break;
+        case 2:
+            start = col-1;
+            break;
+        default:
+            break;
+    }
+    return start;
 }
